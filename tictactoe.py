81a28ba86c6d3428aa69fa267d2064d32e7f78bc
Kristal6113 <157341679+Kristal6113@users.noreply.github.com>
2024-11-14 00:14:29 +0000

    Player X updates board and adds moves used

--- tictactoe.py
+++ tictactoe.py
@@ -17,6 +17,7 @@ print()
 
 winner = ""
 player = "X"
+used_moves=[]
 board=[[1,2,3],
         [4,5,6],
         [7,8,9]]
@@ -30,30 +31,41 @@ print()
 while winner == "":
     if player == "X":
         move = int(input("Player X's Move: "))
-        for row in board:
-            for col in row:
-                if move == 1 and col==1:
-                    col=chr(88)
-                if move ==2 and col==2:
-                    col='X'
-                if move ==3 and col==3:
-                    col='X'
-                if move ==4 and col==4:
-                    col='X'
-                if move ==5 and col==5:
-                    col='X'
-                if move ==6 and col==6:
-                    col='X'
-                if move ==7 and col==7:
-                    col='X'
-                if move ==8 and col==8:
-                    col='X'
-                if move ==9 and col==9:
-                    col='X'
-                print("|",col,"|", end=" ")
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if move == 1 and board[0][0]==1:
+                    board[0][0]='X'
+                    used_moves.append(1)
+                elif move ==2 and board[0][1]==2:
+                    board[0][1]='X'
+                    used_moves.append(2)
+                elif move ==3 and board[0][2]==3:
+                    board[0][2]='X'
+                    used_moves.append(3)
+                elif move ==4 and board[1][0]==4:
+                    board[1][0]='X'
+                    used_moves.append(4)
+                elif move ==5 and board[1][1]==5:
+                    board[1][1]='X'
+                    used_moves.append(5)
+                elif move ==6 and board[1][2]==6:
+                    board[1][2]='X'
+                    used_moves.append(6)
+                elif move ==7 and board[2][0]==7:
+                    board[2][0]='X'
+                    used_moves.append(7)
+                elif move ==8 and board[2][1]==8:
+                    board[2][1]='X'
+                    used_moves.append(8)
+                elif move ==9 and board[2][2]==9:
+                    board[2][2]='X'
+                    used_moves.append(9)
+
+
+                print("|", board[row][col],"|", end=" ")
             print()
-        
         print()
+    print("USED ", used_moves)
 
             
                     

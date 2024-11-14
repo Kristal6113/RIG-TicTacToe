3e4ce8ea0f965e3a07c8b6869f03887c29ca06c8
Kristal6113 <157341679+Kristal6113@users.noreply.github.com>
2024-11-14 00:38:05 +0000

    Player O added with same functionality as Player X

--- tictactoe.py
+++ tictactoe.py
@@ -43,31 +43,89 @@ while winner == "":
                     if move == 1 and board[0][0]==1:
                         board[0][0]='X'
                         used_moves.append(1)
+                        player="O"
                     elif move ==2 and board[0][1]==2:
                         board[0][1]='X'
                         used_moves.append(2)
+                        player="O"
                     elif move ==3 and board[0][2]==3:
                         board[0][2]='X'
                         used_moves.append(3)
+                        player="O"
                     elif move ==4 and board[1][0]==4:
                         board[1][0]='X'
                         used_moves.append(4)
+                        player="O"
                     elif move ==5 and board[1][1]==5:
                         board[1][1]='X'
                         used_moves.append(5)
+                        player="O"
                     elif move ==6 and board[1][2]==6:
                         board[1][2]='X'
                         used_moves.append(6)
+                        player="O"
                     elif move ==7 and board[2][0]==7:
                         board[2][0]='X'
                         used_moves.append(7)
+                        player="O"
                     elif move ==8 and board[2][1]==8:
                         board[2][1]='X'
                         used_moves.append(8)
+                        player="O"
                     elif move ==9 and board[2][2]==9:
                         board[2][2]='X'
                         used_moves.append(9)
+                        player="O"
 
+                    print("|", board[row][col],"|", end=" ")
+                print()
+            print()
+    if player == "O":
+        move = int(input("Player O's Move: "))
+        if move in used_moves:
+            print()
+            print("-----INVALID MOVE-----")
+            print()
+            continue
+        else:
+            for row in range(len(board)):
+                for col in range(len(board[row])):
+                    if move == 1 and board[0][0]==1:
+                        board[0][0]='O'
+                        used_moves.append(1)
+                        player="X"
+                    elif move ==2 and board[0][1]==2:
+                        board[0][1]='O'
+                        used_moves.append(2)
+                        player="X"
+                    elif move ==3 and board[0][2]==3:
+                        board[0][2]='O'
+                        used_moves.append(3)
+                        player="X"
+                    elif move ==4 and board[1][0]==4:
+                        board[1][0]='O'
+                        used_moves.append(4)
+                        player="X"
+                    elif move ==5 and board[1][1]==5:
+                        board[1][1]='O'
+                        used_moves.append(5)
+                        player="X"
+                    elif move ==6 and board[1][2]==6:
+                        board[1][2]='O'
+                        used_moves.append(6)
+                        player="X"
+                    elif move ==7 and board[2][0]==7:
+                        board[2][0]='O'
+                        used_moves.append(7)
+                        player="X"
+                    elif move ==8 and board[2][1]==8:
+                        board[2][1]='O'
+                        used_moves.append(8)
+                        player="X"
+                    elif move ==9 and board[2][2]==9:
+                        board[2][2]='O'
+                        used_moves.append(9)
+                        player="X"
 
                     print("|", board[row][col],"|", end=" ")
                 print()
